0007f61f292662daf446d349a62186c9dfb38171
Promise Tshegofatso magoga <104593732+Promisemagoga@users.noreply.github.com>
2023-01-16 10:39:01 +0200

    added files

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const ejs = require('express');
+const { stringify } = require('nodemon/lib/utils');
+
+//create server
+const app = express();
+app.set('view engine', "ejs");
+app.use('/public', express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//route to home
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//route to about
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+//route to blog
+app.get('/blog', (req, res) => {
+    res.render('blog')
+});
+
+//route to contact
+app.get('/contact', (req, res) => {
+    res.render('contact')
+});
+
+/*app.post('/contact', (req,res) =>{
+    console.log(req);
+});*/
+
+//route to destination
+app.get('/destination', (req, res) => {
+    res.render('destination')
+});
+
+//route to guide
+app.get('/guide', (req, res) => {
+    res.render('guide')
+});
+
+//route to package
+app.get('/package', (req, res) => {
+    res.render('package')
+});
+
+//route to service
+app.get('/service', (req, res) => {
+    res.render('service')
+});
+
+//route to single
+app.get('/single', (req, res) => {
+    res.render('single')
+});
+
+
+app.post('/contact', (req, res) => {
+    const data = req.body
+    const user = new Contact(data)
+    user.save().then((result) => {
+        console.log(result)
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+//route to testimonial
+app.get('/testimonial', (req, res) => {
+    res.render('testimonial')
+});
+//run server
+const port = 6200;
+
+const uri = "mongodb+srv://Promise:mongo2022@cluster0.af7rfrt.mongodb.net/?retryWrites=true&w=majority"
+//connect app to mongodb
+
+mongoose
+    .connect(uri)
+    .then((res) => {
+        console.log("DB connection successful")
+    })
+    .catch((err) => {
+        console.log("error connecting", err)
+    });
+
+//define your schema
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+})
+
+
+const Contact = mongoose.model('Contact', contactSchema)
+//add it to your model
+
+
+app.listen(port, (req, res) => {
+    console.log('server running on port', port);
+});
---
+++ models/contact.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const {schema} = mongoose;
+
+const contactSchema = new mongoose.Schema({
+    name:{
+        type: String
+    },
+    email: {
+        type: String
+    },
+    subject:{
+        type: String
+    },
+    message:{
+        type: String
+    }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
