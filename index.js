0eb0f4f40c440f8e798a6f29319ad75eb313ed9a
Promise Tshegofatso magoga <104593732+Promisemagoga@users.noreply.github.com>
2023-01-22 16:12:31 +0200

    Updated files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const ejs = require('express');
+const ejs = require('ejs');
 const { stringify } = require('nodemon/lib/utils');
 
 //create server
